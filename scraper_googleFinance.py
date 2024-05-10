d6c4b0c5851f734010e6baa84ee9980fc432ccab
Abhay Dixit <abhaydixit227@gmail.com>
2024-05-10 15:48:19 +0530

    Update scraper_googleFinance.py

--- scraper_googleFinance.py
+++ scraper_googleFinance.py
@@ -2,19 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_stock_price(symbol):
-    url = f"https://www.google.com/finance/quote/{symbol}"
+    url = f"https://www.google.com/finance/quote/{symbol}:NSE"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
-    
+    print(soup)
     # Extracting the price
     price = soup.find(class_='YMlKec fxKbKc').text.strip()
     
     return price
 
 if __name__ == "__main__":
-    stock_symbol = input("Enter the stock symbol: ")
+    stock_symbol = input("Enter the stock symbol: ").upper()
     stock_price = get_stock_price(stock_symbol)
+    print("/n")
     print(f"The current price of {stock_symbol} is {stock_price}")
+
